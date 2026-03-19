514f6ac261d06fb1dbfffd73cc3908a0905c894c
Zamlyrics <seanjjsj9898@gmail.com>
2026-03-19 12:42:50 +0200

    Initial commit: ZamLyrics setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-black text-white h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">ZamLyrics 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
